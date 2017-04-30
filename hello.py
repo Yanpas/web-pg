49284ca730e34c6b04011233fdc8e9f4154f9be1
Yan vbx <->
2017-04-30 22:48:29 +0100

    9

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ bind = "0.0.0.0:8080"
 def worker(environ, start_response):
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	for item in environ['QUERY_STRING'].split('&'):
-		yield item + '\n'
+		yield item.encode() + b'\n'
 		#d = item.split('=')
 		#yield "{}:{}".format(d[0], d[1])
 
